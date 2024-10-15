235b3a08578b200b95bf2a864bb95e4ad9b9bb0e
ashik-08 <ashikurrahman.8october@gmail.com>
2024-10-16 02:34:18 +0600

    [feat](task): add all task components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import TaskBoard from "./components/Task/TaskBoard";
 
 const App = () => {
   return (
     <section>
       <Header />
       <Hero />
+      <TaskBoard />
       <Footer />
     </section>
   );
